c80591504e1af4006805fc2d9035c858a4343ea0
Shyamjith Palakkandy <shyamjith.palakkandy@eka.care>
2023-11-30 16:15:41 +0530

    action

--- Sources/SwiftProtoContracts/swift/vitals/commons.cta.pb.swift
+++ Sources/SwiftProtoContracts/swift/vitals/commons.cta.pb.swift
@@ -63,6 +63,16 @@ public struct Commons_Cta_CTAV1 {
   /// Clears the value of `id`. Subsequent reads from it will return its default value.
   public mutating func clearID() {self._id = nil}
 
+  /// cta action
+  public var action: String {
+    get {return _action ?? String()}
+    set {_action = newValue}
+  }
+  /// Returns true if `action` has been explicitly set.
+  public var hasAction: Bool {return self._action != nil}
+  /// Clears the value of `action`. Subsequent reads from it will return its default value.
+  public mutating func clearAction() {self._action = nil}
+
   public var unknownFields = SwiftProtobuf.UnknownStorage()
 
   public init() {}
@@ -70,6 +80,7 @@ public struct Commons_Cta_CTAV1 {
   fileprivate var _params: String? = nil
   fileprivate var _title: String? = nil
   fileprivate var _id: String? = nil
+  fileprivate var _action: String? = nil
 }
 
 #if swift(>=5.5) && canImport(_Concurrency)
@@ -87,6 +98,7 @@ extension Commons_Cta_CTAV1: SwiftProtobuf.Message, SwiftProtobuf._MessageImplem
     6: .same(proto: "params"),
     3: .same(proto: "title"),
     5: .same(proto: "id"),
+    7: .same(proto: "action"),
   ]
 
   public mutating func decodeMessage<D: SwiftProtobuf.Decoder>(decoder: inout D) throws {
@@ -99,6 +111,7 @@ extension Commons_Cta_CTAV1: SwiftProtobuf.Message, SwiftProtobuf._MessageImplem
       case 3: try { try decoder.decodeSingularStringField(value: &self._title) }()
       case 5: try { try decoder.decodeSingularStringField(value: &self._id) }()
       case 6: try { try decoder.decodeSingularStringField(value: &self._params) }()
+      case 7: try { try decoder.decodeSingularStringField(value: &self._action) }()
       default: break
       }
     }
@@ -121,6 +134,9 @@ extension Commons_Cta_CTAV1: SwiftProtobuf.Message, SwiftProtobuf._MessageImplem
     try { if let v = self._params {
       try visitor.visitSingularStringField(value: v, fieldNumber: 6)
     } }()
+    try { if let v = self._action {
+      try visitor.visitSingularStringField(value: v, fieldNumber: 7)
+    } }()
     try unknownFields.traverse(visitor: &visitor)
   }
 
@@ -129,6 +145,7 @@ extension Commons_Cta_CTAV1: SwiftProtobuf.Message, SwiftProtobuf._MessageImplem
     if lhs._params != rhs._params {return false}
     if lhs._title != rhs._title {return false}
     if lhs._id != rhs._id {return false}
+    if lhs._action != rhs._action {return false}
     if lhs.unknownFields != rhs.unknownFields {return false}
     return true
   }
