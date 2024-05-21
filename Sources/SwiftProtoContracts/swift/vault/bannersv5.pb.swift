04884760d3fbfe34748da9d89b16a43ce2523d8f
Vishal Kumar Tuniki <vishal24.tuniki@gmail.com>
2024-05-21 12:07:19 +0530

    Update v5 banner contract (#85)

--- Sources/SwiftProtoContracts/swift/vault/bannersv5.pb.swift
+++ Sources/SwiftProtoContracts/swift/vault/bannersv5.pb.swift
@@ -192,8 +192,8 @@ public struct Vault_Bannersv5_Banner {
       // `Message` and `Message+*Additions` files in the SwiftProtobuf library for
       // methods supported on all messages.
 
-      public var bgAsset: Vault_Bannersv5_Banner.Content.Assets.Img {
-        get {return _bgAsset ?? Vault_Bannersv5_Banner.Content.Assets.Img()}
+      public var bgAsset: Vault_Media_Media {
+        get {return _bgAsset ?? Vault_Media_Media()}
         set {_bgAsset = newValue}
       }
       /// Returns true if `bgAsset` has been explicitly set.
@@ -201,8 +201,8 @@ public struct Vault_Bannersv5_Banner {
       /// Clears the value of `bgAsset`. Subsequent reads from it will return its default value.
       public mutating func clearBgAsset() {self._bgAsset = nil}
 
-      public var topAsset: Vault_Bannersv5_Banner.Content.Assets.Img {
-        get {return _topAsset ?? Vault_Bannersv5_Banner.Content.Assets.Img()}
+      public var topAsset: Vault_Media_Media {
+        get {return _topAsset ?? Vault_Media_Media()}
         set {_topAsset = newValue}
       }
       /// Returns true if `topAsset` has been explicitly set.
@@ -212,66 +212,10 @@ public struct Vault_Bannersv5_Banner {
 
       public var unknownFields = SwiftProtobuf.UnknownStorage()
 
-      public struct Img {
-        // SwiftProtobuf.Message conformance is added in an extension below. See the
-        // `Message` and `Message+*Additions` files in the SwiftProtobuf library for
-        // methods supported on all messages.
-
-        public var image: Vault_Bannersv5_Banner.Content.Assets.Img.OneOf_Image? = nil
-
-        /// Image link for the banner.
-        public var img: String {
-          get {
-            if case .img(let v)? = image {return v}
-            return String()
-          }
-          set {image = .img(newValue)}
-        }
-
-        /// Lottie link for the banner.
-        public var lottie: String {
-          get {
-            if case .lottie(let v)? = image {return v}
-            return String()
-          }
-          set {image = .lottie(newValue)}
-        }
-
-        public var unknownFields = SwiftProtobuf.UnknownStorage()
-
-        public enum OneOf_Image: Equatable {
-          /// Image link for the banner.
-          case img(String)
-          /// Lottie link for the banner.
-          case lottie(String)
-
-        #if !swift(>=4.1)
-          public static func ==(lhs: Vault_Bannersv5_Banner.Content.Assets.Img.OneOf_Image, rhs: Vault_Bannersv5_Banner.Content.Assets.Img.OneOf_Image) -> Bool {
-            // The use of inline closures is to circumvent an issue where the compiler
-            // allocates stack space for every case branch when no optimizations are
-            // enabled. https://github.com/apple/swift-protobuf/issues/1034
-            switch (lhs, rhs) {
-            case (.img, .img): return {
-              guard case .img(let l) = lhs, case .img(let r) = rhs else { preconditionFailure() }
-              return l == r
-            }()
-            case (.lottie, .lottie): return {
-              guard case .lottie(let l) = lhs, case .lottie(let r) = rhs else { preconditionFailure() }
-              return l == r
-            }()
-            default: return false
-            }
-          }
-        #endif
-        }
-
-        public init() {}
-      }
-
       public init() {}
 
-      fileprivate var _bgAsset: Vault_Bannersv5_Banner.Content.Assets.Img? = nil
-      fileprivate var _topAsset: Vault_Bannersv5_Banner.Content.Assets.Img? = nil
+      fileprivate var _bgAsset: Vault_Media_Media? = nil
+      fileprivate var _topAsset: Vault_Media_Media? = nil
     }
 
     public init() {}
@@ -442,8 +386,6 @@ extension Vault_Bannersv5_Banner.Item.OneOf_Result: @unchecked Sendable {}
 extension Vault_Bannersv5_Banner.Content: @unchecked Sendable {}
 extension Vault_Bannersv5_Banner.Content.Highlight: @unchecked Sendable {}
 extension Vault_Bannersv5_Banner.Content.Assets: @unchecked Sendable {}
-extension Vault_Bannersv5_Banner.Content.Assets.Img: @unchecked Sendable {}
-extension Vault_Bannersv5_Banner.Content.Assets.Img.OneOf_Image: @unchecked Sendable {}
 extension Vault_Bannersv5_BannersResponse: @unchecked Sendable {}
 extension Vault_Bannersv5_BannersAPIResponse: @unchecked Sendable {}
 extension Vault_Bannersv5_BannersAPIResponse.OneOf_Result: @unchecked Sendable {}
@@ -686,66 +628,6 @@ extension Vault_Bannersv5_Banner.Content.Assets: SwiftProtobuf.Message, SwiftPro
   public static func ==(lhs: Vault_Bannersv5_Banner.Content.Assets, rhs: Vault_Bannersv5_Banner.Content.Assets) -> Bool {
     if lhs._bgAsset != rhs._bgAsset {return false}
     if lhs._topAsset != rhs._topAsset {return false}
-    if lhs.unknownFields != rhs.unknownFields {return false}
-    return true
-  }
-}
-
-extension Vault_Bannersv5_Banner.Content.Assets.Img: SwiftProtobuf.Message, SwiftProtobuf._MessageImplementationBase, SwiftProtobuf._ProtoNameProviding {
-  public static let protoMessageName: String = Vault_Bannersv5_Banner.Content.Assets.protoMessageName + ".Img"
-  public static let _protobuf_nameMap: SwiftProtobuf._NameMap = [
-    1: .same(proto: "img"),
-    2: .same(proto: "lottie"),
-  ]
-
-  public mutating func decodeMessage<D: SwiftProtobuf.Decoder>(decoder: inout D) throws {
-    while let fieldNumber = try decoder.nextFieldNumber() {
-      // The use of inline closures is to circumvent an issue where the compiler
-      // allocates stack space for every case branch when no optimizations are
-      // enabled. https://github.com/apple/swift-protobuf/issues/1034
-      switch fieldNumber {
-      case 1: try {
-        var v: String?
-        try decoder.decodeSingularStringField(value: &v)
-        if let v = v {
-          if self.image != nil {try decoder.handleConflictingOneOf()}
-          self.image = .img(v)
-        }
-      }()
-      case 2: try {
-        var v: String?
-        try decoder.decodeSingularStringField(value: &v)
-        if let v = v {
-          if self.image != nil {try decoder.handleConflictingOneOf()}
-          self.image = .lottie(v)
-        }
-      }()
-      default: break
-      }
-    }
-  }
-
-  public func traverse<V: SwiftProtobuf.Visitor>(visitor: inout V) throws {
-    // The use of inline closures is to circumvent an issue where the compiler
-    // allocates stack space for every if/case branch local when no optimizations
-    // are enabled. https://github.com/apple/swift-protobuf/issues/1034 and
-    // https://github.com/apple/swift-protobuf/issues/1182
-    switch self.image {
-    case .img?: try {
-      guard case .img(let v)? = self.image else { preconditionFailure() }
-      try visitor.visitSingularStringField(value: v, fieldNumber: 1)
-    }()
-    case .lottie?: try {
-      guard case .lottie(let v)? = self.image else { preconditionFailure() }
-      try visitor.visitSingularStringField(value: v, fieldNumber: 2)
-    }()
-    case nil: break
-    }
-    try unknownFields.traverse(visitor: &visitor)
-  }
-
-  public static func ==(lhs: Vault_Bannersv5_Banner.Content.Assets.Img, rhs: Vault_Bannersv5_Banner.Content.Assets.Img) -> Bool {
-    if lhs.image != rhs.image {return false}
     if lhs.unknownFields != rhs.unknownFields {return false}
     return true
   }
